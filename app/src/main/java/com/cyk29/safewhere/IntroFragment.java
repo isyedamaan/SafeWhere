96b71abd8a1d91b6cce2017f11f1b7244947ed4e
Alyssaatmasava <alyssaatmasava@gmail.com>
2023-12-09 10:48:53 +0800

    Navigation from intro to signup

---
+++ app/src/main/java/com/cyk29/safewhere/IntroFragment.java
@@ -0,0 +1,51 @@
+package com.cyk29.safewhere;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.cyk29.safewhere.databinding.FragmentIntroBinding;
+
+
+public class IntroFragment extends Fragment {
+
+
+    private FragmentIntroBinding binding;
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+
+        ImageButton BtnIntro = view.findViewById(R.id.introBT);
+        View.OnClickListener OCLIntro = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.DestSignup);
+            }
+        };
+        BtnIntro.setOnClickListener(OCLIntro);
+    }
+
+
+
+
+    @Override
+    //IMPORTANT TO BIND FOR IT TO APPEAR ON SCREEN
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentIntroBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+
+
+    }
+}
