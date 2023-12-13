bb7916c2060bd286af022078622973d31271f838
isyedamaan <amaan9927@gmail.com>
2023-12-13 16:57:42 +0800

    Updates

---
+++ app/src/main/java/com/cyk29/safewhere/mapmodule/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cyk29.safewhere.mapmodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cyk29.safewhere.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
