eb75b2a654fe81b45cb4745a5d31637da3724e41
isyedamaan <amaan9927@gmail.com>
2023-12-15 00:42:39 +0800

    Updates on the notification module

--- app/src/main/java/com/cyk29/safewhere/startupmodule/ProfileFragment.java
+++ app/src/main/java/com/cyk29/safewhere/startupmodule/ProfileFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.cyk29.safewhere.R;
 
@@ -57,10 +58,22 @@ public class ProfileFragment extends Fragment {
         }
     }
 
+    Button resetPass;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        
+        resetPass = view.findViewById(R.id.BTResetPassword);
+        resetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+        
+        
+        return view;
     }
 }
