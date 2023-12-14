672cdedf29b4a2879a658ef2c6824f8883c936af
Alyssaatmasava <alyssaatmasava@gmail.com>
2023-12-14 23:41:54 +0800

    Finished profile UI

---
+++ app/src/main/java/com/cyk29/safewhere/startupmodule/ProfileMainActivity.java
@@ -0,0 +1,22 @@
+package com.cyk29.safewhere.startupmodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+
+import com.cyk29.safewhere.R;
+
+public class ProfileMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        NavHostFragment host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.NHFprofile);
+        NavController navController = host.getNavController();
+    }
+
+}
